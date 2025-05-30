adff465f1ba644a15b5a57162a6350a1bb9414b4
Marc SALLA <dvisalla2@gmail.com>
2025-05-30 18:29:23 +0100

    suppression de tous

--- index.js
+++
@@ -1,9 +0,0 @@
-import { app } from './app.js';
-import logger from './middleware/logger.js';
-
-const PORT = process.env.PORT || 3001;
-
-// Start server
-app.listen(PORT, () => {
-  logger.info(`Server is running on port ${PORT}`);
-});
